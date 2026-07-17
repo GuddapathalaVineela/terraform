da358f6f13314c64c4261e02bd6b59d3452ce214
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-17 23:37:42 +0530

    terraform

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -1,34 +1,36 @@
 resource "aws_instance" "terraform" {
-    #count = 10
-    count = length(var.instances)
+    #for_each = var.instances
+    for_each = toset(var.instances)
     ami = "ami-09c813fb71547fc4f"
+    #instance_type = each.value
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
-        Name = var.instances[count.index]
+        #Name = each.key
+        Name = each.value
         Terraform = "true"
-        Project = "roboshop"
     }
 }
 
-resource "aws_security_group" "allow_all"{
-    name = "allow-all"
+resource "aws_security_group" "allow_all" {
+  name   = "allow-all"
 
-    egress {
-        from_port        = 0    # from port 0 to, to port 0 means all ports
-        to_port          = 0
-        protocol         = "-1" # -1 means from all protocols
-        cidr_blocks      = ["0.0.0.0/0"]    # internet
-    }
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
 
-    ingress {
-        from_port        = 0    # from port 0 to, to port 0 means all ports
-        to_port          = 0
-        protocol         = "-1" # -1 means from all protocols
-        cidr_blocks      = ["0.0.0.0/0"]    # internet
-    }
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
 
-    tags = {
-        Name = "allow_all"
-    }
+  tags = {
+    Name = "allow-all"
+  }
+
 }
