ffe01d0f1fa3c1c4ab6ad0377af5af04a1ad5bdd
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-17 22:32:37 +0530

    terraform

--- multi-account/main.tf
+++ multi-account/main.tf
@@ -19,3 +19,24 @@ resource "aws_vpc" "prod" {
         Name = "roboshop-prod"
     }
 }
+
+# Requestor Side
+resource "aws_vpc_peering_connection" "dev_prod" {
+  provider = aws.dev 
+  peer_owner_id = var.prod # Owner is always acceptor
+  peer_vpc_id   = aws_vpc.prod.id # Acceptor
+  vpc_id        = aws_vpc.dev.id
+  tags = {
+    Name = "roboshop-dev-to-prod"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  provider                  = aws.prod
+  vpc_peering_connection_id = aws_vpc_peering_connection.dev_prod.id
+  auto_accept               = true
+
+  tags = {
+    Name = "roboshop-dev-to-prod"
+  }
+}
